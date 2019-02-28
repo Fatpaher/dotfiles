30ec1b45b16a018249deca8f29ee5974424b5f15
Paul Pavlovsky <fatpaher@gmail.com>
2019-02-28 20:36:54 +0200

    Add root file links generator

---
+++ create_links.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+ln -si ~/dotfiles/.rspec ~/.rspec
+ln -si ~/dotfiles/.bash_aliases ~/.bash_aliases
+ln -si ~/dotfiles/.bashrc ~/.bashrc
+ln -si ~/dotfiles/.gitconfig ~/.gitconfig
+ln -si ~/dotfiles/.gitignore ~/.gitignore
+ln -si ~/dotfiles/.irbrc ~/.irbrc
+ln -si ~/dotfiles/.rspec ~/.rspec
+ln -si ~/dotfiles/.rspec ~/.rspec
+ln -si ~/dotfiles/.rubocop.yml ~/.rubocop.yml
+ln -si ~/dotfiles/.vimrc ~/.vimrc
